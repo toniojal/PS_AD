0f81c61560cce2ab9f6e98b0437b9333cb10c619
toniojal <101259166+toniojal@users.noreply.github.com>
2022-03-09 13:48:50 +0200

    Add files via upload

---
+++ Set_new_password_v1.0.ps1
@@ -0,0 +1,42 @@
+﻿
+Function passwdchange {
+    [CmdletBinding()]
+    Param (
+        [parameter(Mandatory=$true, ValueFromPipeline)][String]$user,
+        [parameter(Mandatory=$true)][String]$NewPasswd,
+        [parameter(Mandatory=$true)][String]$NewPasswdAgain
+        
+        )
+}
+
+$user = Read-Host "Anna käyttajän nimi"
+$ExistingADUser = Get-ADUser -Filter "Name -eq '$user' -OR SamAccountName -eq '$user'"
+if ($null -eq $ExistingADUser)
+{
+    Write-Host "Käyttäjää ei löytynyt AD:lta. Scripti sulkeutuu" -ForegroundColor Red
+    return $null
+}
+
+else 
+
+{Write-Output "Käyttäjän tiedot ovat:" $ExistingADUser | fl -Property Name,SamAccountName,Enabled,UserPrincipalName}
+
+$NewPasswd = Read-Host "Anna kelvollinen salasana. Salasanan oltava vähintään 10 merkkiä pitkä, sekä siinä on oltava erikoismerkkejä, isoja kirjaimia ja numeroita"
+$NewPasswdAgain = Read-Host "Anna salasana uudelleen"
+
+if (($NewPasswd -cmatch '[a-z]') `
+-and ($NewPasswd -cmatch '[A-Z]') `
+-and ($NewPasswd -ceq $NewPasswdAgain) `
+-and ($NewPasswd.Length -ge 10) `
+-and ($NewPasswd -contains '!'-or'@'-or'#'-or'%'-or'^'-or'&'-or'$'-or'?'-or'*'))
+
+{
+Set-ADAccountPassword $ExistingADUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $NewPasswd -Force -Verbose) -PassThru -Confirm -WhatIf
+Write-Host "Käyttäjän" $ExistingADUser.SamAccountName "salasana vaihdettu. Uusi salasana on:" $NewPasswd -ForegroundColor Green
+}
+
+else 
+{
+    Write-Host "Salasana ei ole kelvollinen. Tarkista annettu salasanasi." -ForegroundColor Red
+    return $null
+}
---
+++ Set_new_password_v1.1.ps1
@@ -0,0 +1,74 @@
+﻿# Funktiomääritteet jotka voi halutessaan etukäteen määrittää
+Function passwdchange {
+    [CmdletBinding()]
+    Param (
+        [parameter(Mandatory=$true, ValueFromPipeline=$true)][String]$user,
+        [parameter(Mandatory=$true)][String]$NewPasswd,
+        [parameter(Mandatory=$true)][String]$NewPasswdAgain
+        
+        )
+}
+
+#$info = New-ScriptFileInfo -Description UserpasschangeAD -Copyright Toni Ojala
+#$info
+
+# Haetaan käyttäjää AD:lta. Ellei löydy, niin kysyy sitä uudelleen, kunnes käyttäjä löytyy AD:lta.
+do {
+$user = Read-Host "Anna käyttajän nimi tai poistu painamalla 'q'"
+$ExistingADUser = Get-ADUser -Filter "Name -eq '$user' -OR SamAccountName -eq '$user'"
+
+# Mahdollisuus poistua scriptistä painamalla "q"
+if ($user -eq "q")
+{Write-Host "Poistuit scriptistä" -ForegroundColor Red
+return $null}
+
+elseif (!$ExistingADUser)
+{
+    Write-Host "Käyttäjää ei löytynyt AD:lta. Yritä uudelleen" -ForegroundColor Red
+}
+
+else
+
+# Haetaan AD-käyttäjästä tietoja
+{Write-Output "Käyttäjän tiedot ovat:" $ExistingADUser | fl -Property Name,SamAccountName,Enabled,UserPrincipalName}
+}
+
+until ($ExistingADUser)
+
+
+# Syötetään salasana kahteen kertaan
+do {
+$NewPasswd = Read-Host "Anna kelvollinen salasana.
+Salasanan oltava vähintään 10 merkkiä pitkä, sekä siinä on oltava erikoismerkkejä, isoja kirjaimia ja numeroita.
+Poistu painamalla 'q'"
+
+# Mahdollisuus poistua scriptistä painamalla "q"
+if ($NewPasswd -eq "q")
+{Write-Host "Poistuit scriptistä" -ForegroundColor Red
+return $null}
+
+else {}
+$NewPasswdAgain = Read-Host "Anna salasana uudelleen"
+
+# Määritetään salasanan ehdot. Vähintään 1 pieni ja iso kirjain, vähintään 1 erikoismerkki, pituus vähintään 10 merkkiä.
+# Syötettyjen salasanojen on täsmättävä keskenään (katso edellinen kohta)
+if ($t=(($NewPasswd -cmatch '[a-z]') `
+-and ($NewPasswd -cmatch '[A-Z]') `
+-and ($NewPasswd -ceq $NewPasswdAgain) `
+-and ($NewPasswd.Length -ge 10) `
+-and ($NewPasswd -contains '!'-or'@'-or'#'-or'%'-or'^'-or'&'-or'$'-or'?'-or'*')))
+
+# Vaihdetaan salasana, mikäli ehdot täyttyvät
+{
+Set-ADAccountPassword $ExistingADUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $NewPasswd -Force -Verbose) -PassThru -Confirm -WhatIf
+Write-Host "Käyttäjän" $ExistingADUser.SamAccountName "salasana vaihdettu. Uusi salasana on:" $NewPasswd -ForegroundColor Green
+}
+
+else
+# Mikäli salasana ei ole kelvollinen, ilmoittaa siitä ja kysyy salasanaa uudelleen 
+{
+    Write-Host "Salasana ei ole kelvollinen. Tarkista annettu salasanasi." -ForegroundColor Red
+}
+
+}
+until ($t)
