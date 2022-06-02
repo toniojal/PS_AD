a700eaa07f0ee5c15d731ce77679868970578284
toniojal <101259166+toniojal@users.noreply.github.com>
2022-06-02 12:57:19 +0300

    Add files via upload

---
+++ Show_office_licensetype.ps1
@@ -0,0 +1,7 @@
+﻿
+do {
+
+$mail = Read-Host "Anna sähköpostiosoite tai poistu kirjoittamalla 'exit'"
+Get-MsolUser -UserPrincipalName $mail | Format-List DisplayName,Licenses
+}
+until ($mail -eq "exit")
